b03239be1ffa841bd0db025817d2d9b2d42563b2
Manjunath Patil <btcpatil11@gmail.com>
2022-09-29 23:17:52 +0530

    day3

---
+++ sb101_miniProject/src/com/miniProject/home/Welcome.java
@@ -0,0 +1,17 @@
+package com.miniProject.home;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome the online banking system");
+		
+		System.out.println("--------------------------------------");
+		System.out.println("Choose the option");
+		System.out.println("1. Login as accountant");
+		System.out.println("2. Login as customer");
+		
+
+	}
+
+}
